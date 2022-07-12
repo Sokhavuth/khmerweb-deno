f7d2720f2ef24035cf5f023d83ce2d77e6fd32f0
Sokhavuth <vuthdevelop@gmail.com>
2022-07-12 22:41:35 +0700

    Make it better

--- models/connectdb.js
+++ models/connectdb.js
@@ -2,10 +2,14 @@
 
 import { config, MongoClient } from "../deps.ts"
 
-const { DATABASE_URI, DB_NAME } = await config()
+async function getdb(){
+  const { DATABASE_URI, DB_NAME } = await config()
 
-const client = await new MongoClient()
-await client.connect(DATABASE_URI)
-const mydb = client.database(DB_NAME);
+  const client = await new MongoClient()
+  await client.connect(DATABASE_URI)
+  const mydb = client.database(DB_NAME)
+  console.log(mydb)
+  return mydb
+}
 
-export default mydb
+export default await getdb()
