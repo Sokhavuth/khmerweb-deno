99addbf9bd3f739a1e07a437345cbc4bf04e079c
Sokhavuth <vuthdevelop@gmail.com>
2022-07-12 15:51:14 +0700

    Make it better

--- models/connectdb.js
+++ models/connectdb.js
@@ -6,6 +6,5 @@ const env = await config()
 
 const client = new MongoClient()
 await client.connect(env.DATABASE_URI)
-  
-const mydb = client.database(env.DB_NAME)
-export default mydb
+
+export default await client.database(env.DB_NAME)
