f65714d71326e7aa28341a1b4f57f1cd4ed916b4
Sokhavuth <vuthdevelop@gmail.com>
2022-07-12 16:01:28 +0700

    Make it better

--- models/connectdb.js
+++ models/connectdb.js
@@ -4,7 +4,7 @@ import { config, MongoClient } from "../deps.ts"
 
 const env = await config()
 
-const client = new MongoClient()
+const client = await new MongoClient()
 await client.connect(env.DATABASE_URI)
 const mydb = client.database(env.DB_NAME)
 
